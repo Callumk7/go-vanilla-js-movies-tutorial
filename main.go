35b6dc2ce3b7ba5a3488a196a30bfdbdcf8c4a8c
Callum Kloos <Callumkloos@gmail.com>
2025-07-03 15:42:46 +0100

    creating go models

---
+++ main.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"log"
+	"net/http"
+
+	"callumkloos.dev/reelingit/logger"
+)
+
+func initLogger() *logger.Logger {
+	logInstance, err := logger.NewLogger("movie.log")
+
+	if err != nil {
+		log.Fatalf("failed to init logger %v", err)
+	}
+
+	defer logInstance.Close()
+	return logInstance
+
+}
+
+func main() {
+
+	logInstance := initLogger()
+
+	http.Handle("/", http.FileServer(http.Dir("public")))
+
+	const addr = "localhost:8080"
+	err := http.ListenAndServe(addr, nil)
+	if err != nil {
+		log.Fatalf("Server failed: %v", err)
+		logInstance.Error("Server failed", err)
+	}
+}
