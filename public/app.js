d97787eb54a1944525d582d2535ea5996aa01ca9
Callum Kloos <Callumkloos@gmail.com>
2025-07-04 16:07:32 +0100

    web components

--- public/app.js
+++ public/app.js
@@ -1,8 +1,11 @@
+import { API } from "./services/api.js";
+
 window.app = {
     search: (event) => {
         event.preventDefault();
 
         const q = document.querySelector("input[type=search]").value;
         // TODO: Complete the search
-    }
+    },
+    api: API 
 }
